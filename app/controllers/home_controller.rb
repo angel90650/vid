452bdc0393bc3a36a6c6c8c17199f98704908133
Angel Castro <angel90650@gmail.com>
2019-02-21 10:56:53 -0800

    video player working with rails video tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,25 @@ class HomeController < ApplicationController
   def about
   end
 
+  def video
+    path = Rails.root.join('public/s10.mp4')
+    size = File.size(path)
+    status_code = 206
+    if !request.headers["Range"]
+      status_code = 200 # 200 OK
+      offset = 0
+      length = File.size(path)
+    else
+      status_code = 206 # 206 Partial Content
+      bytes = Rack::Utils.byte_ranges(request.headers, size)[0]
+      offset = bytes.begin
+      length = bytes.end - bytes.begin
+    end
+      response.header["Accept-Ranges"] = "bytes"
+      response.header["Content-Range"] = "bytes #{bytes.begin}-#{bytes.end}/#{size}" if bytes
+
+      send_data IO.binread(path, length, offset)
+    else
+      raise ActionController::MissingFile, "Cannot read file #{path}."
+    end
 end
